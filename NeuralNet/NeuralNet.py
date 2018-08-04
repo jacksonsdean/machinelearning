8e1c987fd3fad1a9dabdda0f1e5678cd835db98c
Jackson Dean <jacksonsdean@gmail.com>
2018-08-03 22:48:04 -0600

    maybe working net? need to run it on the server with many iterations to test. added saving

--- NeuralNet/NeuralNet.py
+++ NeuralNet/NeuralNet.py
@@ -3,15 +3,17 @@ import csv
 from pandas import read_csv
 
 class NeuralNetwork():
+    neurons = 512
     def __init__(self):
         # Seed the np.random number generator, so it generates the same numbers
         # every time the program runs.
         np.random.seed(1)
 
-        # We model a single neuron, with 3 input connections and 1 output connection.
-        # We assign np.random weights to a 3 x 1 matrix, with values in the range -1 to 1
+        # We model a single neuron, with 784 input connections and 10 output connection.
+        # We assign np.random weights to a 784 x 1 matrix, with values in the range -1 to 1
         # and mean 0.
-        self.synaptic_weights = 2 * np.random.random((784, 1)) - 1
+        self.synaptic_weights = 2 * np.random.random((784, self.neurons)) - 1
+        # self.synaptic_weights_1 = 2 * np.random.random((10, 1)) - 1
 
     # The Sigmoid function, which describes an S shaped curve.
     # We pass the weighted sum of the inputs through this function to
@@ -49,75 +51,77 @@ class NeuralNetwork():
         # Pass inputs through our neural network (our single neuron).
         return self.__sigmoid(np.dot(inputs, self.synaptic_weights))
 
+    def predict(self, input):
+        self.think(input)
+        return np.argmax(self.synaptic_weights[-1])
 
-def get_training_data_input():
-    training_data = np.zeros((784, 42000))
-    csv_file = './data/train.csv'
-    dataFrame = read_csv(csv_file, header=0)
-    # for each column
-    for col in dataFrame.columns:
-        # for value in dataFrame[col][1:]:
-        np.append(training_data, dataFrame[col])
-    print(training_data.shape)
-    return training_data # the first row is just headers
 
-def get_training_data_output():
-    labels = []
-    csv_file = './data/train.csv'
-    columns = read_csv(csv_file)
-    labels = columns.label
-    return labels # the first row is just labels
 
-def get_test_input():
-    i = 0
-    test_input = [1]
-    with open('./data/test.csv', 'r') as csvfile:
-        reader = csv.reader(csvfile, delimiter=',')
-        headers = next(reader)
-        for row in reader:
-            if i > 1000:
-                break
-            i += 1
-            img_data = []
-            for val in row[1:]:
-                f = float(val)
-                img_data.append(f)
-            test_input.append(img_data)
-    return test_input # the first row is just labels
+    def get_training_data_input(self):
+        training_data = np.zeros((self.neurons, 784))
+        csv_file = './data/train.csv'
+        dataFrame = read_csv(csv_file, header=0)
+        # for each column
+        for col in dataFrame.columns:
+            # for value in dataFrame[col][1:]:
+            np.append(training_data, dataFrame[col])
+        return training_data # the first row is just headers
+
+    def get_training_data_output(self):
+        labels = np.zeros((self.neurons))
+        csv_file = './data/train.csv'
+        dataFrame = read_csv(csv_file)
+        labels_df = dataFrame.label
+        np.append(labels, labels_df)
+        return labels # the first row is just labels
+
+    def get_test_data(self):
+        result = dict()
+        test_case = 500
+
+        one_img_arr = []
+        csv_file = './data/train.csv'
+        dataFrame = read_csv(csv_file, header=0)
+
+        result["label"] = dataFrame[dataFrame.columns[0]][test_case]
+        for col in dataFrame.columns[1:]:
+            one_img_arr.append(dataFrame[col][test_case])
+
+        result["arr"] = one_img_arr
+        return result
 
 if __name__ == "__main__":
 
     #Intialise a single neuron neural network.
-    neural_network = NeuralNetwork()
-
-    # print("np.random starting synaptic weights: ")
-    # print(len(neural_network.synaptic_weights))
+    n_net = NeuralNetwork()
 
     # The training set. We have 4 examples, each consisting of 3 input values
     # and 1 output value.
     # training_set_inputs = array([[0, 0, 1], [1, 1, 1], [1, 0, 1], [0, 1, 1]])
 
-    input = np.array(get_training_data_input())
-    output = np.array(get_training_data_output())
-    # for i in inspect.getmembers(input.size):
-        # print(i[0])
+    input = n_net.get_training_data_input()
+    output = n_net.get_training_data_output()
 
-    print("input size:",input.shape)
-    print("output size:",output.shape)
+    print("input size:", input.shape)
+    print("output size:", output.shape)
 
     training_set_inputs = input
     training_set_outputs = output
 
     # Train the neural network using a training set.
-    # Do it 10,000 times and make small adjustments each time.
-    neural_network.train(training_set_inputs, training_set_outputs, 10)
+    n_net.train(training_set_inputs, training_set_outputs, 10000)
 
     with open("out.txt", "w") as file:
-        for w in neural_network.synaptic_weights:
+        for w in n_net.synaptic_weights:
             file.write(str(w) + "\n")
-    print("New synaptic weights after training: ")
-    print(neural_network.synaptic_weights)
+
+    print("Saving new synaptic weights after training: ")
+    np.save(file='results/weights.npy', arr=n_net.synaptic_weights)
+
+    test_data = n_net.get_test_data()
+
+    print("\nTEST PREDICTION:\nActual:",test_data["label"] , "Prediction:", n_net.predict(test_data["arr"]))
 
     # Test the neural network with a new situation.
-    print("Considering test data: ")
-    print(neural_network.think(get_test_input()))
+    # print("Considering test data: ")
+    # print(n_net.think(get_test_input()))
